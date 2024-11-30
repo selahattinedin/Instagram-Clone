295ed8c1d90ea167eca512cdeab197c24be661d3
Selahattin Edin <selahattinedin1@gmail.com>
2024-11-30 23:24:45 +0300

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Selahattin EDİN on 30.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
